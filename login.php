c32ff8fae32836d1632be507d3f673bc35c93204
L1ucas <69647369+L1ucas@users.noreply.github.com>
2023-04-27 10:05:57 -0300

    Mudado para php

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="tela_login">
+        <form action="" class="form_login">
+            <div class="campo_input">
+                <label for="">Nome</label>
+                <input type="text" id="input_nome">
+            </div>
+            <div class="campo_input">
+                <label for="">Senha</label>
+                <input type="password" id="input_senha">
+            </div>
+            <button type="submit" class="btn-primary">Entrar</button>
+        </form>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+
+</html>

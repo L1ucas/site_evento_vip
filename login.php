cac442f2455006dbd9b84c6c04d699ed6cc55b3f
L1ucas <1lucascode@gmail.com>
2023-04-27 10:44:49 -0300

    Tela de login WIP

--- login.php
+++ login.php
@@ -13,19 +13,33 @@
 
 <body>
     <div class="tela_login">
-        <form action="" class="form_login">
-            <div class="campo_input">
-                <label for="">Nome</label>
-                <input type="text" id="input_nome">
+    <form action="" class="form_login">
+        <div class="container">
+            <div class="row">
+
+                <div class="col">
+                    <label for="">Nome</label>
+                    <input type="text" id="input_nome" class="form-control">
+                </div>
             </div>
-            <div class="campo_input">
-                <label for="">Senha</label>
-                <input type="password" id="input_senha">
+            <div class="row">
+                <div class="col">
+                    <label for="">Senha</label>
+                    <input type="password" id="input_senha" class="form-control">
+                </div>
             </div>
-            <button type="submit" class="btn-primary">Entrar</button>
-        </form>
+            <div class="row">
+                <div class="col">
+                    <button type="submit" class="btn btn-primary">Entrar</button>
+
+                </div>
+            </div>
+        </div>
+
+    </form>
     </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>

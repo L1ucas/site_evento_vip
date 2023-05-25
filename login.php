7e41aece5278b9abc703d340d36e2a7280dac8bc
L1ucas <1lucascode@gmail.com>
2023-05-25 13:33:22 -0300

    Stufff gettting dooone

--- login.php
+++ login.php
@@ -13,32 +13,35 @@
 
 <body>
     <div class="tela_login">
-    <form action="" class="form_login">
-        <div class="container">
-            <div class="row">
+        <form action="" class="form_login">
+            <div class="container">
+                <div class="row">
 
-                <div class="col">
-                    <label for="">Nome</label>
-                    <input type="text" id="input_nome" class="form-control">
+                    <div class="col">
+                        <label for="">Nome</label>
+                        <input type="text" id="input_nome" class="form-control">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <label for="">Senha</label>
+                        <input type="password" id="input_senha" class="form-control">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <button type="submit" class="btn btn-primary">Entrar</button>
+                    </div>
+                </div>
+                <div class="row">
+                    Esqueceu sua senha
+
                 </div>
             </div>
-            <div class="row">
-                <div class="col">
-                    <label for="">Senha</label>
-                    <input type="password" id="input_senha" class="form-control">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <button type="submit" class="btn btn-primary">Entrar</button>
 
-                </div>
-            </div>
-        </div>
-
-    </form>
+        </form>
     </div>
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
